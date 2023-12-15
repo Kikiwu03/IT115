a329face5f8a744f06ebf71e7a15b9c052ad5612
Kiki Wu <kiki.wu03@icloud.com>
2023-12-15 09:06:56 -0800

    first commit

--- JSON Library.py
+++ JSON Library.py
@@ -1,13 +1,13 @@
 import json
 
 
-#Create the data dictionary
+# Create the data dictionary
 
 data = {
     'name': 'Kiki Wu',
     'age':20,
     'city':'Seattle,WA',
-    'interests': ['dogs','coding','foodie']
+    'interests': ['dogs', 'coding', 'foodie'],
     'is_student': True
 }
 
@@ -15,4 +15,25 @@ data = {
 with open('data.json', 'w') as json_file:
     json.dump(data, json_file, indent=4)
 
-#making changes
+
+print('Data has been written to data.json')
+
+# Opens JSON file, Reading data from the file
+with open('data.json', 'r') as json_file:
+  
+    # loads the data
+    loaded_data = json.load(json_file)
+
+print('Data loaded from data.json')
+print(loaded_data)
+
+
+## Change age to 27, calls interests 
+loaded_data['age'] = 27
+loaded_data['interests'].append('They secretly let me do what I want because they dont have any other coder')
+
+# Write the modifications to the file
+with open('data.json', 'w') as json_file:
+    json.dump(loaded_data, json_file, indent=4)
+
+print('Modified data to the data.json file')
