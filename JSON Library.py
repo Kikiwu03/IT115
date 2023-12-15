a6b190611b7b86c72f87a049e23582ed5a26148a
Kiki Wu <kiki.wu03@icloud.com>
2023-12-15 08:33:33 -0800

    first commit

---
+++ JSON Library.py
@@ -0,0 +1,16 @@
+import json
+
+
+#Create the data dictionary
+
+data = {
+    'name': 'Kiki Wu',
+    'age':20,
+    'city':'Seattle,WA',
+    'interests': ['dogs','coding','foodie']
+    'is_student': True
+}
+
+# Writing data to a JSON file
+with open('data.json', 'w') as json_file:
+    json.dump(data, json_file, indent=4)
