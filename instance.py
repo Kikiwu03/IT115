0fa7421f36a8effdf48ffc6b39424163ad4969b6
Kiki Wu <kiki.wu03@icloud.com>
2023-12-17 19:56:45 -0800

    token

--- instance.py
+++ instance.py
@@ -6,9 +6,11 @@ from ec2_metadata import ec2_metadata
 
 print(ec2_metadata.region)
 print(ec2_metadata.instance_id)
+print(ec2_metadata.public_ipv4)
 
+load_dotenv()
 
-client = discord.Bot()
+client = discord.Client()
 token = str(os.getenv('TOKEN'))
 
 
@@ -26,22 +28,34 @@ async def on_message(message):
     
     print(f'Message {user_message} by {username} on {channel}')
 
+    discord_channels = ["random", "general", "chat"]
+
     
     if message.author == client.user:
         return 
     
     
-    if channel == "blah-blah":
-        if user_message.lower() == "boomer?" or user_message.lower() == "boomer?":
-            await message.channel.send(f"Sooner! {username} Your EC2 Data: {ec2_metadata.region}")
-            return 
-        
-        elif user_message.lower() == "hello?":
-            await message.channel.send(f'Sooner! {username}')
+    if channel == discord_channels:
+        try: 
+
+            if user_message.lower() == "hello" or user_message.lower() == "hi?":
+                await message.channel.send(f"Hi There! {username} Your EC2 Data: {ec2_metadata.region}")
+                return 
             
-         
-        elif user_message.lower() == "EC2 Data":
-            await message.channel.send("Your instance data is" + ec2_metadata.region)
-        
+            elif user_message.lower() == "hello world":
+                await message.channel.send("hello user")
+                
+            
+            elif user_message.lower() == "tell me about my server":
+                await message.channel.send(f"Here is your IP Address: {ec2_metadata.public_ipv4}, Your EC2 Region: {ec2_metadata.region}, and Avaliability zone: {ec2_metadata.availability}")
+                return
+            
+            elif user_message.lower() == "bye":
+                await message.channel.send(f"Bye {username} Your EC2 Data: {ec2_metadata.region}")
+
+        except Exception as e:
+            print(f"Error:" {e}")
+            await message.channel.send("Unable to respond due to an error")
+
 
 client.run(token)
